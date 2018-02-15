f628add8a5f7b7e461eaf28a335ce98005b6efa9
Anthony Cameron <ar.cameron@gmail.com>
2018-02-15 00:48:31 -0500

    lab tweaks

--- src/pages/lab.js
+++ src/pages/lab.js
@@ -2,13 +2,46 @@ import React from 'react'
 import Link from 'gatsby-link'
 import StandardPage from '../components/StandardPage'
 import ReadableSection from '../components/ReadableSection'
+import LabFeature from '../components/LabFeature'
+
+import img_muvee from './lab/muvee/img/movies-index.jpeg';
+import img_srpg from './lab/webgl-srpg-engine/img/srpg1.png';
 
 const IndexPage = () => (
   <StandardPage>
     <ReadableSection>
-      <h1>Lab</h1>
-      <Link to='/lab/muvee'>Muvee</Link>
-      <Link to='/lab/webgl-srpg-engine'>WebGL SRPG</Link>
+      <h1 className="page-title">Lab</h1>
+      <LabFeature
+        title='muvee'
+        img={img_muvee}
+        to='/lab/muvee'
+        description='This project is something like Netflix, but run on a server in your own home. It aims to be an open source alternative to Plex.'>
+      </LabFeature>
+      <LabFeature
+        title='WebGL SRPG Engine'
+        img={img_srpg}
+        to='/lab/webgl-srpg-engine'
+        description='A university project, created for CS488 (Introduction to Computer Graphics) during Fall 2011 at the University of Waterloo as a final project.' />
+      <LabFeature
+        title='muvee'
+        img={img_muvee}
+        to='/lab/muvee'
+        description='Like Netflix, but for your home' />
+      <LabFeature
+        title='WebGL SRPG Engine'
+        img={img_srpg}
+        to='/lab/webgl-srpg-engine'
+        description='Gfx project' />
+      <LabFeature
+        title='muvee'
+        img={img_muvee}
+        to='/lab/muvee'
+        description='Like Netflix, but for your home' />
+      <LabFeature
+        title='WebGL SRPG Engine'
+        img={img_srpg}
+        to='/lab/webgl-srpg-engine'
+        description='Gfx project' />
     </ReadableSection>
   </StandardPage>
 );
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -24,13 +24,9 @@ import './portfolio.scss';
 const IndexPage = () => (
   <StandardPage>
     <ReadableSection>
-      <h1>Front-end Work</h1>
-      <p>I specialize in front-end development, and I believe I can make any design a reality. I'm not a designer by trade, but I like to dabble and design via CSS.</p>
-      <p>For many of these projects, I was the sole developer. For a few, I worked with my good friend and colleague <a href="http://kaizhiwei.com/" target="_blank">Kaizhi Wei</a>.</p>
-      <p>Generally, the main focus was PSD2HTML. Some were flat sites, others were driven by WordPress. I used HAML and SASS were used for rapid prototyping, and image spritesheets whenever applicable.</p>
-      <p>Many sites had needed custom widgets. I've coded dozens of custom slideshows in my time, interactive maps, and several full-spread responsive image-based landing pages.</p>
-      <p>I'm experienced with integration of external services – yelp, Facebook, iHomeFinder, and other 3rd party weather services.</p>
-      <p>These days, I've fleshed out more in terms of back-end development. I'd still consider myself primarily a front-end developer, as I'd say that's where the majority of my talents lie. There's just something so satisfying about seeing things come to life in front of your eyes!</p>
+      <h1 className="page-title">Front-end Work</h1>
+      <p>I specialize in front-end development, and I believe I can make any design a reality. I'm not a designer by trade, but I like to dabble and design via CSS. I like to think I have an eye for aesthetics.</p>
+      <p>Below you'll see some work I was involved with back in my freelancing days.</p>
     </ReadableSection>
     <ReadableSection>
       <ImageGrid>
