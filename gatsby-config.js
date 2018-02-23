ef62501e103034bdb4cd3ead8a904c14160879bf
Anthony Cameron <ar.cameron@gmail.com>
2018-02-22 23:59:30 -0500

    add favicon plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,27 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'anthonycameron.com',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: false,
+          twitter: true,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
   ],
 };
