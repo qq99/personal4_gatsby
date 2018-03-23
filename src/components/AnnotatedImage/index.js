ceaa77c93ff0cde37f5a21eb1972b5096d6be069
Anthony Cameron <ar.cameron@gmail.com>
2018-03-22 22:55:49 -0400

    adjust border-radius on annotated image/canvas

--- src/components/AnnotatedImage/index.js
+++ src/components/AnnotatedImage/index.js
@@ -1,26 +1,18 @@
 import React from "react";
 
-import "./annotated-image.css";
+import "./annotated-image.scss";
 
 class AnnotatedImage extends React.Component {
-
   render() {
     return (
       <figure className="annotated-image">
-        { this.props.children }
-        <figcaption style={{display: this.props.caption ? "block" : "none"}}>{this.props.caption}</figcaption>
+        {this.props.children}
+        <figcaption style={{ display: this.props.caption ? "block" : "none" }}>
+          {this.props.caption}
+        </figcaption>
       </figure>
     );
   }
-
-  // render() {
-  //   return (
-  //     <figure className="annotated-image">
-  //       <img src={ this.props.src } />
-  //       <figcaption style={{display: this.props.caption ? "block" : "none"}}>{this.props.caption}</figcaption>
-  //     </figure>
-  //   );
-  // }
 }
 
 export default AnnotatedImage;
