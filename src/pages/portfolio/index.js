70e01e8db41509edfce800aee556e6e7b252f9a5
Anthony Cameron <ar.cameron@gmail.com>
2018-03-22 23:17:29 -0400

    move 2 portfolio images out of render

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -15,12 +15,19 @@ const IndexPage = ({ data }) => (
     <Helmet title="Portfolio - Anthony Cameron" />
     <ReadableSection>
       <Typography.PageTitle>Front-end Work</Typography.PageTitle>
-      <p>I specialize in front-end development, and I believe I can make any design a reality. I'm not a designer by trade, but I like to dabble and design via CSS. I like to think I have an eye for aesthetics.</p>
-      <p>Below you'll see some work I was involved with back in my freelancing days.</p>
+      <p>
+        I specialize in front-end development, and I believe I can make any
+        design a reality. I'm not a designer by trade, but I like to dabble and
+        design via CSS. I like to think I have an eye for aesthetics.
+      </p>
+      <p>
+        Below you'll see some work I was involved with back in my freelancing
+        days.
+      </p>
     </ReadableSection>
     <ReadableSection>
       <ImageGrid>
-        { data.portfolioImages.edges.map((image) => (
+        {data.portfolioImages.edges.map(image => (
           <AnnotatedImage>
             <Img sizes={ image.node.sizes } />
           </AnnotatedImage>
@@ -34,13 +41,12 @@ export default IndexPage;
 
 export const query = graphql`
   query PortfolioQuery {
-    portfolioImages: allImageSharp(filter: { id: { regex: "/portfolio/img/tile*/" } }) {
+    portfolioImages: allImageSharp(
+      filter: { id: { regex: "/portfolio/img/tile*/" } }
+    ) {
       edges {
         node {
-          sizes(
-            maxWidth: 350
-            quality: 80
-          ) {
+          sizes(maxWidth: 350, quality: 80) {
             ...GatsbyImageSharpSizes_tracedSVG
           }
         }
