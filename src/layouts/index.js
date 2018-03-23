ad50344c143cdeecf2ac4926962daae6d0e25e1f
Anthony Cameron <ar.cameron@gmail.com>
2018-03-23 00:13:30 -0400

    betta' meta

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,18 +7,56 @@ import Footer from "../components/Footer";
 import "./index.css";
 import "./styles.scss";
 
+import metaImg from "../favicon.png";
+
+const metaTitle = "Anthony Cameron";
+const metaDescription =
+  "Everything you need to know about Anthony Cameron: A collection of personal projects, featuring WebGL, GLSL shaders, JavaScript, C++.  A sample of my portfolio.  My resume.  My philosophy.";
+
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
       title="Anthony Cameron"
       meta={ [
-        { name: "author", content: "Anthony Cameron" },
+        { name: "author", content: metaTitle },
         {
           name: "description",
-          content:
-            "Everything you need to know about Anthony Cameron: A collection of personal projects, featuring WebGL, GLSL shaders, JavaScript, C++.  A sample of my portfolio.  My resume.  My philosophy."
+          content: metaDescription
         },
-        { name: "viewport", content: "width=device-width,initial-scale=1" }
+        { name: "viewport", content: "width=device-width,initial-scale=1" },
+        { name: "og:title", content: metaTitle },
+        {
+          name: "og:description",
+          content: metaDescription
+        },
+        {
+          name: "og:image",
+          content: metaImg
+        },
+        {
+          name: "twitter:title",
+          content: metaTitle
+        },
+        {
+          name: "twitter:description",
+          content: metaDescription
+        },
+        {
+          name: "twitter:card",
+          content: "summary_large_image"
+        },
+        {
+          name: "twitter:site",
+          content: "@dontqq"
+        },
+        {
+          name: "twitter:creator",
+          content: "@dontqq"
+        },
+        {
+          name: "twitter:image",
+          content: metaImg
+        }
       ] }
     />
     <div className="main-container">
